f96cf9662629f2c39b9312308f5f18f4012012f7
akshatkumar <akshatk806@gmail.com>
2025-02-25 21:00:50 +0530

    setting the value of a signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,14 @@ export class AppComponent implements OnInit {
   theme = signal("light");   // writable signal (we get the value using getter funtion)
 
   ngOnInit(): void {
+    // this.theme.set("dark")    // if you set the value directly you simply overrided 
+
+    // updating is another alternative you can use to set the value of signal by using the current value
+    this.theme.update(currentValue => currentValue === "light" ? "dark" : "light"); 
+
+    // you can also use current value in set method (but here is two method calls)
+    // this.theme.set(this.theme() === "light" ? "dark" : "light");
+
     document.body.className = this.theme();   // getter funtion of signal
   }
 }
