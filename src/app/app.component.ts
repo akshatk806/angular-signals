e36a7b1c8ced78b19f82719c98d8a977bb40ecad
akshatkumar <akshatk806@gmail.com>
2025-02-25 20:53:10 +0530

    creating an angular signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-signals-project';
+
+  // signal for toggle
+  theme = signal("light");   // writable signal (we get the value using getter funtion)
+
+  ngOnInit(): void {
+    document.body.className = this.theme();   // getter funtion of signal
+  }
 }
