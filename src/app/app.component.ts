b75311b40aaa16abb7f7ae2bd9498fad4372fecf
akshatkumar <akshatk806@gmail.com>
2025-02-25 21:22:24 +0530

    creating an effect for signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { Signal } from '@angular/core';
+import { Signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,22 @@ import { Signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular-signals-project';
 
   // signal for toggle
   theme = signal("light");   // writable signal (we get the value using getter funtion)
 
+  label = this.theme();
+
+  constructor() {
+    // creating effect, effect is code that runs when the value of signal changes
+    effect(() => {
+      this.label = this.theme();   // whenever we change the value of signal from anywhere from code this effect will here runs. This is the dependency we have created
+    });
+  }
+
+  /*
   ngOnInit(): void {
     // this.theme.set("dark")    // if you set the value directly you simply overrided 
 
@@ -24,4 +34,17 @@ export class AppComponent implements OnInit {
 
     document.body.className = this.theme();   // getter funtion of signal
   }
+  */
+
+  toggleDarkMode() : void {
+    this.theme.update(currentValue => currentValue === "light" ? "dark" : "light"); 
+  }
+  /* Explanation -
+
+    I click on toggle button that runs the toggleDarkMode() method that will change the value of signal
+    and as we created a effect inside our constructor the effects knows we are using the signal 
+    so whenever we changes signal we will have to run this effect, so label is updated.
+    
+    The effect is executed over and over again whenever angular detects that we have the value of signal
+  */
 }
