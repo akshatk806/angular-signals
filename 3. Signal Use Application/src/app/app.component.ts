d3017bd6d035e3eb1ae33015259827a733359e1c
akshatkumar <akshatk806@gmail.com>
2025-02-25 23:26:09 +0530

    use of signals while searching products

--- 3. Signal Use Application/src/app/app.component.ts
+++ 3. Signal Use Application/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'signal-demo-app';
+  products = signal([
+    { id: 1, name: "Milk", price: 60 },
+    { id: 2, name: "Bread", price: 30 },
+    { id: 3, name: "Egg", price: 10 }
+  ]);
+
+  // filter products by name
+  filterName = signal("");
+
+  filteredProducts = computed(() => {
+    return this.products().filter(product => product.name.toLowerCase().includes(this.filterName().toLowerCase()))
+  });
+
+  changeFilter(event: Event) {
+    let newFilterName = (event.target as HTMLInputElement).value;
+    this.filterName.set(newFilterName);
+  }
 }
